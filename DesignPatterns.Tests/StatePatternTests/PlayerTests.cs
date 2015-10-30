d4d278ab16b7edf7a60721d46e154242cf470b3a
vignesh <rvignesh@outlook.com>
2015-10-30 16:32:53 +0800

    move tests into new folder

---
+++ DesignPatterns.Tests/StatePatternTests/PlayerTests.cs
@@ -0,0 +1,65 @@
+﻿using DesignPatterns.StatePattern;
+using NUnit.Framework;
+
+namespace DesignPatterns.Tests.StatePatternTests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        private Player _Player;
+
+        [Test]
+        public void Test_CreatingAPlayerWithName()
+        {
+            Assert.That(_Player.Name,Is.EqualTo("mark"));
+        }
+
+        [Test]
+        public void Test_NewPlayerState_IsHealth()
+        {
+            Assert.That(_Player.State,Is.EqualTo(PlayerState.Healthy));
+        }
+
+        [Test]
+        public void Test_WhenNewPlayerIsHit_StateBecomesUnhealthy()
+        {
+            _Player.Hit();
+
+            Assert.That(_Player.State,Is.EqualTo(PlayerState.UnHealthy));
+        }
+
+        [Test]
+        public void Test_WhenUnhealthyPlayerIsHealed_StateBecomesHealth()
+        {
+            _Player.Heal();
+
+            Assert.That(_Player.State,Is.EqualTo(PlayerState.Healthy));
+        }
+
+        [Test]
+        public void Test_WhenUnhealthPlayerIsHit_StateBecomesDead()
+        {
+            _Player.Hit();
+            _Player.Hit();
+
+            Assert.That(_Player.State,Is.EqualTo(PlayerState.Dead));
+        }
+
+        [Test]
+        public void Test_WhePlayerIsDead_HealingDoesNotWork()
+        {
+            _Player.Hit();
+            _Player.Hit();
+            
+            _Player.Heal();
+
+            Assert.That(_Player.State,Is.EqualTo(PlayerState.Dead));
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _Player = new Player("mark");   
+        }
+    }
+}
