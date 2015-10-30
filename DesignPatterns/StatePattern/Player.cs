954bfb3987b13603d7b447d99e63eb80e408b91c
vignesh <rvignesh@outlook.com>
2015-10-30 16:13:26 +0800

    init repo

---
+++ DesignPatterns/StatePattern/Player.cs
@@ -0,0 +1,32 @@
+﻿namespace DesignPatterns.StatePattern
+{
+    public class Player
+    {
+        public Player(string name)
+        {
+            Name = name;
+            State = PlayerState.Healthy;
+        }
+
+        public string Name { get; private set; }
+        public PlayerState State { get; set; }
+
+        public void Hit()
+        {
+            State = State == PlayerState.UnHealthy ? PlayerState.Dead : PlayerState.UnHealthy;
+        }
+
+        public void Heal()
+        {
+            State = State == PlayerState.Dead ? PlayerState.Dead : PlayerState.Healthy;
+        }
+    }
+
+    public enum PlayerState
+    {
+        Unknown,
+        Healthy,
+        UnHealthy,
+        Dead
+    }
+}
